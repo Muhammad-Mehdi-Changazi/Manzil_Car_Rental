7a6114b7d2406d47c74e60c6d6fe5b42928a074a
Muhammad Mehdi Changezi <113912243+Muhammad-Mehdi-Changazi@users.noreply.github.com>
2025-04-13 12:30:06 +0500

    files uploaded

---
+++ app/Routes/Routes.ts
@@ -0,0 +1,6 @@
+export type RootStackParamList = {
+  index: undefined;
+  LoginScreen: undefined;
+  Home: undefined;
+  HotelAdmin: { username: string }; // Type for HotelAdmin screen, where username is passed
+};
---
+++ app/_layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Stack } from 'expo-router';
+import { AuthProvider } from './contexts/authcontext';  // Adjusted path
+import { MenuProvider } from 'react-native-popup-menu';
+
+export default function RootLayout() {
+  return (
+    <MenuProvider>
+      <AuthProvider>
+        <View style={styles.container}>
+          {/* <Header />  Uncomment if needed */}
+          <Stack screenOptions={{ headerShown: false }} />
+        </View>
+      </AuthProvider>
+    </MenuProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
